0037190c23d7e815bb8df385da7497811bdf2dc3
Malte Mader <mader@zbh.uni-hamburg.de>
2014-02-18 17:43:04 +0100

    Update GT install script. Use latest stable GT version v1.5.2.

--- install_genometools.sh
+++ install_genometools.sh
@@ -2,14 +2,20 @@
 
 git clone https://github.com/genometools/genometools.git
 
+cd genometools
+
+git checkout -b v1.5.2 v1.5.2
+
+cd ..
+
 cp -R gt_db_api/* genometools/
 
 cd genometools/
 
 if test `uname -m` = "x86_64"; then
-	make -j 64bit=yes with-mysql=yes;
+	make 64bit=yes with-mysql=yes;
 fi
 
 if test `uname -m` = "i386"; then
-	make -j with-mysql=yes;
+	make with-mysql=yes;
 fi
