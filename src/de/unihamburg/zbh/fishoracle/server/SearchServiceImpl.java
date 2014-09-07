ecc5fa51aeeb4b2bf9609dc8cd3b61a8ec079580
Malte Mader <mader@zbh.uni-hamburg.de>
2014-09-07 12:46:01 +0200

    Revert url.

--- src/de/unihamburg/zbh/fishoracle/server/SearchServiceImpl.java
+++ src/de/unihamburg/zbh/fishoracle/server/SearchServiceImpl.java
@@ -427,7 +427,7 @@ public class SearchServiceImpl extends RemoteServiceServlet implements SearchSer
 		} else {
 			throw new Exception("Only one Track allowed.");
 		}
-		return servletContext + url;
+		return url;
 	}
 	
 	@Override
